e1add007fa664c159155f7b76df8c23fab263bbf
MiguelHort <miguel.d.hort@gmail.com>
2024-05-03 17:46:22 -0300

    Icone de trofeu

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+
+import Icontrofeu from '../assets/Icon/Icontrofeu';
+
 import '../style/components/Card.scss';
 
 function Card({ textName, imgLugar }) {
     return (
         <a className='card-popular' href="">
+            <div className='icon-trofeu'>
+                <Icontrofeu></Icontrofeu>
+            </div>
             <img src={imgLugar} alt="Imagem do local mais popular para sair" />
-            <div>
+            <div className='name-local'>
                 <p>{textName}</p>
             </div>
         </a>
